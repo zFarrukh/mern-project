5f4c99bcb5e1cefc53e233f3d813706e1e170ad7
zFarrukh <diplomatfarruh@gmail.com>
2022-06-29 16:17:00 +0500

    add shared components and work on styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,8 +4,20 @@ import UsersList from '../components/UsersList';
 
 function Users() {
   const USERS = [
-    { id: 1, image: 'url', name: 'John', places: 3 },
-    { id: 2, image: 'url', name: 'Sara', places: 4 },
+    {
+      id: 1,
+      image:
+        'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg',
+      name: 'John',
+      places: 3,
+    },
+    {
+      id: 2,
+      image:
+        'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg',
+      name: 'Sara',
+      places: 4,
+    },
   ];
 
   return <UsersList items={USERS} />;
